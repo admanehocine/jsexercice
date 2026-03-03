7941d59c8c87d05de20c3d1640f8f570e34a2078
admanehocine <admanehocine05@gmail.com>
2026-03-03 16:34:08 +0100

    commit

---
+++ Test.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+public class Test {
+   public static void main(String[] args) { 
+      ArrayList<int> noms = new ArrayList<>();
+      noms.add("Ali"); 
+      noms.add("Sara");
+      nom.add("Yacine"); 
+     for (int i = 0; j < nom.size(); i++) { 
+       System.out.println(noms.get(i)); 
+     }
+}
--- test.js
+++
@@ -1,9 +0,0 @@
-const Personne = require("./Personne.js");
-
-let p1 = new Personne(1, "Alice", "Dupont");
-let p2 = new Personne(2, "Bob", "Martin");
-let p3 = new Personne(3, "Charlie", "Durand");
-
-p1.direBonjour();
-p2.direBonjour();
-p3.direBonjour();
